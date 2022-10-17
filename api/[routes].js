2b6166c0a06ecb55e2f97198d88d3d1c54455696
RamyEB <ramy.elb@gmail.com>
2022-10-17 10:02:49 +0200

    change api

--- api/[routes].js
+++ api/[routes].js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const manifest = JSON.parse(fs.readFileSync(path.join(process.cwd(), '/manifests/test.json'), 'utf-8'));
 
+function getManifest(path) {
+  return JSON.parse(fs.readFileSync(path.join(process.cwd(), path), 'utf-8'));
+
+}
+
 export default function handler(req, res) {
-  return res.json(manifest);
+  return res.json(getManifest('/manifests/'+req.body.path));
 }

d6d5d416caf3069c9db224ef9246a6f5a3616e02
RamyEB <ramyelb@gmail.com>
2022-10-17 11:21:09 +0200

    add clg

--- api/[routes].js
+++ api/[routes].js
@@ -4,14 +4,13 @@ const path = require('path');
 
 function getManifest(path) {
   return JSON.parse(fs.readFileSync(path.join(process.cwd(), path), 'utf-8'));
-
 }
 
 export default function handler(req, res) {
   const queryObject = url.parse(req.url, true).query;
-  const { routes: fileName } = request.query;
-  console.log(fileName)
+  const { routes } = request.query;
+  console.log(routes)
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json(getManifest('/manifests/'+fileName+'.json'));
+  res.json(getManifest('/manifests/'+routes+'.json'));
 }
